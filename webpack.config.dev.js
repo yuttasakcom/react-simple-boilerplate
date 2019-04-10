4b809805d1746e494007fee82cee35785e5d2fee
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-04-10 17:28:12 +0700

    updated webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,21 +1,35 @@
 const path = require("path");
-const resolve = dir => path.resolve(__dirname, dir);
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
+const resolve = dir => path.resolve(__dirname, dir);
+
 const config = {
-  devtool: "eval",
+  devtool: "cheap-module-source-map",
   entry: "./src/index.js",
   output: {
     path: resolve("dist"),
+    publicPath: "/",
     filename: "bundle.js",
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false,
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        use: ["babel-loader"],
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
